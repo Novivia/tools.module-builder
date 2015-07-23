f89c492a0f29f4f7ebb7065d6bd1740020a392d4
Mathieu M-Gosselin <mathieumg@atx33.com>
2015-07-22 21:38:00 -0400

    Worked on publication tool and adjusted build tool a little.

--- cli.js
+++ cli.js
@@ -1,12 +1,12 @@
-import argv from "yargs";
-import {build} from "./lib";
+import yargs from "yargs";
+import {build, publication} from "./lib";
 import getPkgInfo from "pkginfo-json5";
 
 const pkgInfo = getPkgInfo(module);
 
-function buildCommand(yargs) {
+function buildCommand(args) {
   const argv = (
-    yargs
+    args
     .example(
       "$0 build -b 'util/**/*.js'", "Build the matching files using Babel"
     )
@@ -24,13 +24,6 @@ function buildCommand(yargs) {
       "Force to use the Babel runtime even if it can't be found during " +
       "compilation",
     )
-
-    .alias("s", "silent")
-    .describe("s", "Don't output anything")
-
-    .alias("v", "verbose")
-    .describe("v", "Be explicit about everything")
-
 
     .help("h")
     .alias("h", "help")
@@ -50,8 +43,6 @@ function buildCommand(yargs) {
   }
 
   const isSilent = !!argv.silent;
-  // console.log("Package filters:", packagePatterns);
-  // console.log("Babel filters:", babelPatterns);
 
   build.make({
     babelPatterns,
@@ -70,12 +61,75 @@ function buildCommand(yargs) {
   });
 }
 
-argv
+function publishCommand(args) {
+  const argv = (
+    args
+    .example(
+      "$0 publish myModule-v1.2.3-8e19b7c8-d5d6-4e60-87fb-9c8aceadae51.tar.gz",
+      "Publish the specified module",
+    )
+
+    .alias("c", "clean")
+    .default("c", true)
+    .describe("c", "Remove the file after publication")
+
+    .alias("m", "most-recent")
+    .describe(
+      "m",
+      "If no file is specified, use the most recent matching the pattern",
+    )
+
+    .help("h")
+    .alias("h", "help")
+    .argv
+  );
+
+  const isSilent = !!argv.silent;
+
+  let publishFile;
+  if (argv._.length > 1) {
+    publishFile = argv._[1];
+  }
+
+  publication.publish({
+    file: publishFile,
+    mostRecent: !!argv.mostRecent,
+    silent: isSilent,
+    verbose: !!argv.verbose,
+  }).then(publishedFile => {
+    if (!publishedFile) {
+      return;
+    }
+
+    if (!isSilent) {
+      console.log("Package published!");
+    }
+
+    if (argv.clean) {
+      build.clean(publishedFile);
+    }
+  }).catch(e => {
+    if (!isSilent) {
+      console.error("Publication failed:", e);
+    }
+  });
+}
+
+yargs
 .usage("Usage: $0 <command> [options]")
-.command("build", "Build the module to a .tar.gz file", buildCommand)
-.demand(1)
+
+.alias("s", "silent")
+.describe("s", "Don't output anything")
+
+.alias("v", "verbose")
+.describe("v", "Be explicit about everything")
+
 .help("h")
 .alias("h", "help")
+
+.command("build", "Build the module to a .tar.gz file", buildCommand)
+.command("publish", "Publish the module from a .tar.gz file", publishCommand)
+.demand(1)
+
 .epilog(`Version ${pkgInfo.version}`)
 .argv;
-
